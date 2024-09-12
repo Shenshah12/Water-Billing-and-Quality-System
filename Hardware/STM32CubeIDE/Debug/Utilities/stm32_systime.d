0ba2e9c751389a06b88a64dcff28490818830f2c
Muhammad Jahangir <88369789+Shenshah12@users.noreply.github.com>
2024-09-12 16:49:25 +0500

    Delete Hardware directory

--- Hardware/STM32CubeIDE/Debug/Utilities/stm32_systime.d
+++
@@ -1,4 +0,0 @@
-Utilities/stm32_systime.o: \
- C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_systime.c \
- C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_systime.h
-C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_systime.h:
--- Hardware/STM32CubeIDE/Debug/Utilities/stm32_tiny_vsnprintf.d
+++
@@ -1,4 +0,0 @@
-Utilities/stm32_tiny_vsnprintf.o: \
- C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_tiny_vsnprintf.c \
- C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_tiny_vsnprintf.h
-C:/Users/User/STM32CubeIDE/workspace_1.14.0/LoRaWAN_End_Node/Utilities/misc/stm32_tiny_vsnprintf.h:
